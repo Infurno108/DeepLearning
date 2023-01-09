d972f4f327b794a1a64995beafc46a3169e558b6
Flint <oliverflintrose@gmail.com>
2023-01-09 13:00:10 -0600

    Worked on 1.1 (not finished yet)

--- Chapter1/Chapter1.py
+++ Chapter1/Chapter1.py
@@ -1,2 +1,11 @@
 import numpy as np
-#
+
+a = np.array([0,1,0,1])
+b = np.array([0,1,1,0])
+#building a XOR gate
+#expected: 0,0,1,1
+
+def xor(x: nparray, y: ndarray):
+    return (x + y)
+
+print(xor(a,b))
