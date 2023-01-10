a4e3af64b070f5622c0fa7fba33a79ddb93e85d4
Flint <oliverflintrose@gmail.com>
2023-01-10 00:45:43 -0600

    midnight work because insomnia sucks.

--- Chapter1/Chapter1.py
+++ Chapter1/Chapter1.py
@@ -1,11 +1,25 @@
 import numpy as np
-
+#1.1
 a = np.array([0,1,0,1])
 b = np.array([0,1,1,0])
-#building a XOR gate
+#building an and gate
 #expected: 0,0,1,1
+def AND(x, y):
+    return (x * y)
+#print(AND(a,b))
 
-def xor(x: nparray, y: ndarray):
-    return (x + y)
+#1.2
+def f(x):
+    return ((5*(x*x)) - 4)
 
-print(xor(a,b))
+def deriv(function, x, h = 0.000001):
+    return ((function(x + h) - function(x))/h)
+
+#print(int(deriv(f, 2))) #expected 20
+
+def derivx5(function, x, h = 0.000001):
+    array = np.array(10)
+    for i in range(0, 10):
+        np.append(array, deriv(function, x + i - 5))  
+    return array
+print(derivx5(f,2))
