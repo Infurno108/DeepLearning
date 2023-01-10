c0aee8cd2008322a1e41bd45fb8a531402f1a3fb
Flint <oliverflintrose@gmail.com>
2023-01-10 15:37:17 -0600

    Finished 1.2

--- Chapter1/Chapter1.py
+++ Chapter1/Chapter1.py
@@ -7,19 +7,19 @@ b = np.array([0,1,1,0])
 def AND(x, y):
     return (x * y)
 #print(AND(a,b))
-
 #1.2
 def f(x):
     return ((5*(x*x)) - 4)
-
+#f(x) = 5x^2 -4 
+#f'(x) = 10x
 def deriv(function, x, h = 0.000001):
-    return ((function(x + h) - function(x))/h)
-
+    return round((function(x + h) - function(x))/h)
 #print(int(deriv(f, 2))) #expected 20
+def derivx5(function, x, h = 0.00000000001):
+    bap = np.array([0,0,0,0,0,0,0,0,0,0])
+    for i in range(0,10):
+        bap[i] = deriv(function, x - 5 + i)
+    return bap
+#print(derivx5(f,2))
+#1.3
 
-def derivx5(function, x, h = 0.000001):
-    array = np.array(10)
-    for i in range(0, 10):
-        np.append(array, deriv(function, x + i - 5))  
-    return array
-print(derivx5(f,2))
